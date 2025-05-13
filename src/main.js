269666ccf7a8df055030354425cfd449734931dc
Roman Marchenko <kirkusik@gmail.com>
2025-05-13 09:24:13 +0300

    Initial commit (history reset)

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./styles/main.scss";
+
+import Swiper from "swiper";
+import { Navigation, Pagination, Scrollbar, EffectFade } from "swiper/modules";
+
+Swiper.use([Navigation, Pagination, Scrollbar, EffectFade]);
+
+document.addEventListener("DOMContentLoaded", () => {});
